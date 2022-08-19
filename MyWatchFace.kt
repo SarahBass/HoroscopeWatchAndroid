e4f875b46c9f162efd3efd022e74ab2dad433882
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-08-18 19:18:42 -0700

    Update MyWatchFace.kt

--- MyWatchFace.kt
+++ MyWatchFace.kt
@@ -590,6 +590,7 @@ class MyWatchFace : CanvasWatchFaceService() {
             val dayOfTheWeekLong: String = sdf1.format(d)
             val monthOfYear: String = sdf2.format(d)
             val dayOfMonth: String = sdf3.format(d)
+
             val year4digits: String = sdf4.format(d)
             val fullDateSpaces: String = sdf5.format(d)
             val timeSpecific : String = sdf6.format(d)
@@ -779,31 +780,63 @@ class MyWatchFace : CanvasWatchFaceService() {
             var result =getPlanetEvent().filter { it.isDigit() }
             return Integer.parseInt(result)
         }
+
+        private fun getPlanetEventNumber2(): Int {
+            var result =getPlanetEvent2().filter { it.isDigit() }
+            return Integer.parseInt(result)
+        }
+
+        private fun filterPlanetNews(): String {
+            val d = Date()
+            val sdf3 = SimpleDateFormat("d")
+            val dayOfMonth: String = sdf3.format(d)
+
+            val filteredString =
+                if (getPlanetEventNumber() >= Integer.parseInt(dayOfMonth)) {
+                    getPlanetEvent()
+                }else {"none"}
+
+            return filteredString
+        }
+
+        private fun filterPlanetNews2(): String {
+            val d = Date()
+            val sdf3 = SimpleDateFormat("d")
+            val dayOfMonth: String = sdf3.format(d)
+
+            val filteredString =
+                if (getPlanetEventNumber2() >= Integer.parseInt(dayOfMonth)) {
+                    getPlanetEvent2()
+                }else {"none"}
+
+            return filteredString
+        }
+
         private fun getPlanetEvent(): String {
             val d = Date()
             val sdf0 = SimpleDateFormat("yyyy MMMM")
             val yearMonth: String = sdf0.format(d)
             val planetOpposition =
                 when(yearMonth){
-                    "2022 January" -> "Jupiter in Pisces January 1st" //Mercury Visible at Sunset
+                    "2022 January" -> "Jupiter in Pisces until January 30th" //Mercury Visible at Sunset
                     "2022 February" -> "Venus Brightest on February 9" //Pluto returns. This happens only once 248 years
-                    "2022 March" -> "March 20 - March Equinox" //March 18: Worm Moon
-                    "2022 April" -> "Mercury will be visible at Sunrise" //April 16: Pink Moon
-                    "2022 May" -> "none"
+                    "2022 March" -> "March 20 - March Equinox"
+                    "2022 April" -> "Mercury will be visible at Sunrise until 30th"
+                    "2022 May" -> "none 0"
                     "2022 June" -> "June 28: Neptune begins retrograde motion" //Mercury Visible at Sunrise
                     "2022 July" -> "July 28: Jupiter begins retrograde motion" // Pluto at Opposition 20 Jul 2022
                     "2022 August" -> "Saturn in Opposition on August 14" //August 24, 2022: Uranus begins retrograde motion
                     "2022 September" -> "Jupiter at opposition on September 26" //Septemper 16, 2022: Neptune at opposition
                     "2022 October" -> "Saturn ends retrograde motion" //Mars in Retrograde October 30, 2022
                     "2022 November" -> "November 23: Jupiter ends retrograde motion" // 2022 Uranus opposition – November 9
-                    "2022 December" -> "Mars at Opposition on December 8" //Dec 21: December Solstice
+                    "2022 December" -> "December 15 - Geminids Meteor Shower" //Dec 21: December Solstice
 
-                    "2023 January" -> "Uranus ends retrograde motion" //Jan 7, 2023: Inferior conjunction Mercury
+                    "2023 January" -> "Uranus ends retrograde motion January 22nd" //Jan 7, 2023: Inferior conjunction Mercury
                     "2023 February" -> "February 16: Saturn in conjunction with the sun"
-                    "2023 March" -> "Neptune at solar conjunction" //Uranus at solar conjunction
+                    "2023 March" -> "Neptune at solar conjunction March 15th" //Uranus at solar conjunction
                     "2023 April" -> "April 11: Jupiter at solar conjunction"
                     "2023 May" -> "Venus Brightest on May 12" // Mercury Visible at Sunrise
-                    "2023 June" -> "Neptune begins retrograde motion"
+                    "2023 June" -> "none 0"
                     "2023 July" -> "Pluto at Opposition : July 22 "
                     "2023 August" -> "Saturn at Opposition on August 27" // Uranus begins retrograde motion
                     "2023 September" -> "September 19 : Neptune at opposition"
@@ -811,34 +844,35 @@ class MyWatchFace : CanvasWatchFaceService() {
                     "2023 November" -> "Jupiter opposition – November 2" // 2023 Uranus opposition – November 13
                     "2023 December" -> "December 14 - Geminids Meteor Shower"
 
-                    "2024 January" -> "none"
-                    "2024 February" -> "none"
-                    "2024 March" -> "Mercury Visible at Sunset"
-                    "2024 April" -> "none"
-                    "2024 May" -> "Mercury Visible at Sunrise"
+                    "2024 January" -> "none 0"
+                    "2024 February" -> "none 0"
+                    "2024 March" -> "Mercury Visible at Sunset unil 30th"
+                    "2024 April" -> "none 0"
+                    "2024 May" -> "Mercury Visible at Sunrise until 30th"
                     "2024 June" -> "Venus at superior solar conjunction : June 04 "
                     "2024 July" -> "Pluto at Opposition : July 23 " //Mercury visible at Sunset
-                    "2024 August" -> "none"
+                    "2024 August" -> "none 0"
                     "2024 September" -> "Saturn is in Opposition on September 8" //Mercury visible at Sunrise
-                    "2024 October" -> "none"
+                    "2024 October" -> "none 0"
                     "2024 November" -> "Uranus opposition – November 16" //Mercury visible at sunset
-                    "2024 December" -> "Jupiter opposition – December 7"
+                    "2024 December" -> "December 14 - Geminids Meteor Shower"
 
                     "2025 January" -> "Jupiter opposition– January 10" // "Mars is in Opposition on January 16th"
                     "2025 February" -> "Venus at greatest brightness: Feb 16"
-                    "2025 March" -> "Mercury visible at Sunset"
-                    "2025 April" -> "none"
-                    "2025 May" -> "none"
-                    "2025 June" -> "none"
+                    "2025 March" -> "Mercury visible at Sunset until 30th"
+                    "2025 April" -> "none 0"
+                    "2025 May" -> "none 0"
+                    "2025 June" -> "none 0"
                     "2025 July" -> "Pluto at Opposition : July 25"
-                    "2025 August" -> "none"
+                    "2025 August" -> "none 0"
                     "2025 September" -> "Saturn is in Opposition on September 25"
-                    "2025 October" -> "none"
+                    "2025 October" -> "none 0"
                     "2025 November" -> "Uranus opposition – November 21"
                     "2025 December" -> "December 14 - Geminids Meteor Shower"
 
-                    else -> "none"
+                    else -> "none 0"
                 }
+
             return planetOpposition
 
         }
@@ -860,7 +894,7 @@ class MyWatchFace : CanvasWatchFaceService() {
                     "October" -> "Harvest Moon"
                     "November" -> "Beaver Moon"
                     "December" -> "Cold Moon"
-                    else -> "None"
+                    else -> "none 0"
                 }
             return planetOpposition}
 
@@ -870,41 +904,41 @@ class MyWatchFace : CanvasWatchFaceService() {
             val yearMonth: String = sdf0.format(d)
             val planetOpposition =
                 when(yearMonth){
-                    "2022 January" -> "Mercury Visible at Sunset"
-                    "2022 February" -> "none"
-                    "2022 March" -> "none"
-                    "2022 April" -> "none"
+                    "2022 January" -> "Mercury Visible at Sunset until 30th"
+                    "2022 February" -> "none 0"
+                    "2022 March" -> "Spring Equinox on March 20th"
+                    "2022 April" -> "none 0"
                     "2022 May" -> "May 6: Eta Aquarid Meteors"
-                    "2022 June" -> "Mercury Visible at Sunrise"
+                    "2022 June" -> "June 20: Summer Solstice"
                     "2022 July" -> "Pluto at Opposition Jul 20"
                     "2022 August" -> "August 24 : Uranus begins retrograde motion"
-                    "2022 September" -> "Septemper 16: Neptune at opposition"
+                    "2022 September" -> "Fall Equinox September 22nd"
                     "2022 October" -> "Mars in Retrograde October 30"
                     "2022 November" -> "Uranus at opposition – November 9"
-                    "2022 December" -> "Dec 21: December Solstice"
+                    "2022 December" -> "Dec 21: Winter Solstice"
 
                     "2023 January" -> "Jan 7: Inferior conjunction Mercury"
-                    "2023 February" -> "none"
-                    "2023 March" -> "Uranus at solar conjunction"
-                    "2023 April" -> "none"
-                    "2023 May" -> "Mercury Visible at Sunrise"
+                    "2023 February" -> "none 0"
+                    "2023 March" -> "Spring Equinox on March 20th"
+                    "2023 April" -> "none 0"
+                    "2023 May" -> "Mercury Visible at Sunrise until 30th"
                     "2023 June" -> "June 21: Summer Solstice"
-                    "2023 July" -> "none"
-                    "2023 August" -> "Uranus begins retrograde motion"
+                    "2023 July" -> "none 0"
+                    "2023 August" -> "none 0"
                     "2023 September" -> "Fall Equinox September 23rd"
-                    "2023 October" -> "none"
+                    "2023 October" -> "none 0"
                     "2023 November" -> "Uranus at opposition – November 13"
                     "2023 December" -> "Winter Solstice on Dec 21st"
 
                     "2024 January" -> "January 4 - Quadrantids Meteor Shower"
-                    "2024 February" -> "none"
+                    "2024 February" -> "none 0"
                     "2024 March" -> "Spring Equinox March 19th"
                     "2024 April" -> "April 8: Total Solar Eclipse parts of USA"
                     "2024 May" -> "May 7 - Eta Aquarids Meteor Shower"
                     "2024 June" -> "Summer Solstice Jun 20th"
-                    "2024 July" -> "Mercury visible at Sunset"
+                    "2024 July" -> "Mercury visible at Sunset until 30th"
                     "2024 August" -> "August 13 - Perseids Meteor Shower"
-                    "2024 September" -> "Mercury visible at Sunrise"
+                    "2024 September" -> "Fall Equinox September 24"
                     "2024 October" -> "October 22 - Orionids Meteor Shower"
                     "2024 November" -> "Mercury visible at Sunset"
                     "2024 December" -> "Winter Solstice Dec 21st"
@@ -922,7 +956,7 @@ class MyWatchFace : CanvasWatchFaceService() {
                     "2025 November" -> "November 5th - Taurids Meteor Shower"
                     "2025 December" -> "Winter Solstice : December 21st"
 
-                    else -> "none"
+                    else -> "none 0"
                 }
             return planetOpposition
 
@@ -976,7 +1010,7 @@ class MyWatchFace : CanvasWatchFaceService() {
                 getPlanetEvent().contains("Pluto")-> "pluto"
                 getPlanetEvent().contains("Meteor")-> "shower"
                 getPlanetEvent().contains("meteor")-> "shower"
-                else -> "none"
+                else -> "none 0"
             }
 
             return planetType
@@ -997,7 +1031,7 @@ class MyWatchFace : CanvasWatchFaceService() {
                 getPlanetEvent().contains("Buck") -> "moonbanimal"
                 getPlanetEvent().contains("Flower") -> "moonpink"
                 getPlanetEvent().contains("Beaver") -> "moonbeaver"
-                else -> "none"
+                else -> "none 0"
             }
 
             return planetType
@@ -1031,8 +1065,8 @@ class MyWatchFace : CanvasWatchFaceService() {
                 getPlanetEvent().contains("Pluto")-> "pluto"
                 getPlanetEvent().contains("Meteor")-> "shower"
                 getPlanetEvent().contains("meteor")-> "shower"
-                getPlanetEvent().contains("None")-> "none"
-                else -> "none"
+                getPlanetEvent().contains("None")-> "none 0"
+                else -> "none 0"
             }
 
             return planetType2
@@ -1057,8 +1091,8 @@ class MyWatchFace : CanvasWatchFaceService() {
                 getPlanetEvent3().contains("Pluto")-> "pluto"
                 getPlanetEvent3().contains("Meteor")-> "sun"
                 getPlanetEvent3().contains("meteor")-> "sun"
-                getPlanetEvent3().contains("None")-> "none"
-                else -> "none"
+                getPlanetEvent3().contains("None")-> "none 0"
+                else -> "none 0"
             }
 
             return planetType3
